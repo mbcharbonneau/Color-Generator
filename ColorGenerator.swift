d36a2461e22b9b3d13647ed8dcf5336401dd2b31
Marc Charbonneau <marc@mbcharbonneau.com>
2015-01-06 16:03:13 -0800

    Empty project.

---
+++ ColorGenerator.swift
@@ -0,0 +1,9 @@
+//
+//  ColorGenerator.swift
+//  Color Generator
+//
+//  Created by mbcharbonneau on 1/6/15.
+//  Copyright (c) 2015 Downtown Software House. All rights reserved.
+//
+
+import Foundation
